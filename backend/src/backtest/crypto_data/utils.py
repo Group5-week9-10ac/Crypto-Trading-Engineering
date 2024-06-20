73758a405a5c9461417c962b9bd8b51b012e711d
MoraaOntita <moraaontita1844@gmail.com>
2024-06-20 21:32:24 +0300

    Modified the makefile, dataloader and utils to include the LSTM task, Created new files to help in LSTM time-series forecasting model.

--- backend/src/backtest/crypto_data/utils.py
+++ backend/src/backtest/crypto_data/utils.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from sklearn.model_selection import train_test_split as sk_train_test_split
 
 def create_folder(folder):
     """
@@ -12,7 +13,7 @@ def create_folder(folder):
 
 def save_data(data, filename, folder='data'):
     """
-    Save data as CSV file.
+    Save data as a CSV file.
 
     Args:
     - data (pd.DataFrame): Data to be saved.
@@ -22,3 +23,19 @@ def save_data(data, filename, folder='data'):
     create_folder(folder)
     filepath = os.path.join(folder, filename)
     data.to_csv(filepath, index=False)
+
+def load_data(filepath):
+    """
+    Load data from a CSV file.
+
+    Args:
+    - filepath (str): Path to the CSV file.
+
+    Returns:
+    - pd.DataFrame: Loaded data.
+    """
+    if os.path.exists(filepath):
+        return pd.read_csv(filepath)
+    else:
+        print(f"File {filepath} does not exist.")
+        return None
